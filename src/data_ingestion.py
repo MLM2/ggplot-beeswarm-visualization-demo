e262d46a0c02154592fec3999be088878b5b06b1
Mike McKeever <mike.l.mckeever@gmail.com>
2025-07-07 00:11:07 -0400

    Finalized data ingestion script

---
+++ src/data_ingestion.py
@@ -0,0 +1,10 @@
+ggplot-beeswarm-visualization-demo
+├── data
+├── notebooks
+├── powerbi
+├── src
+│   ├── data_ingestion.py
+│   ├── ggplot_plotting.py
+│   └── beeswarm_plotting.py
+├── README.md
+└── requirements.txt
